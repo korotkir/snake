1ccf0bc4095275aa5e56089e8602747663f011f5
Kirill <ekoroteev27@yandex.ru>
2020-10-28 17:50:59 +0300

    start

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('worked')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+    
+ctx.fillStyle = 'rgb(0,0,0)'
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
