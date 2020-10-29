8133c5b572677841783917af793c1e56fc526a9f
Kirill <ekoroteev27@yandex.ru>
2020-10-29 17:34:32 +0300

    add control key

--- app.js
+++ app.js
@@ -8,6 +8,39 @@ let y = 30
 let dx = 2
 let dy = 2
 
+let upPress = false
+let downPress = false
+let leftPress = false
+let rightPress = false
+
+document.addEventListener('keydown', KeyDownHandler, false)
+document.addEventListener('key', KeyUpHandler, false)
+
+// Кнопки
+function KeyDownHandler(e) {
+    if(e.keyCode == 38) {
+        upPress = true
+    }else if(e.keyCode == 40){
+        downPress = true
+    }else if(e.keyCode == 37){
+        leftPress = true
+    }else if(e.keyCode == 39){
+        rightPress = true
+    }
+} 
+
+function KeyUpHandler(e) {
+    if(e.keyCode == 38) {
+        upPress = false
+    }else if(e.keyCode == 40){
+        downPress = false
+    }else if(e.keyCode == 37){
+        leftPress = false
+    }else if(e.keyCode == 39){
+        rightPress = false
+    }
+}
+
 // Функция отвечающая за отрисовку змейки
 function snake() {
     ctx.beginPath()
@@ -33,13 +66,22 @@ function pass() {
 }
 
 function direction() {
-    //x += -dx
-    y += dy
+    //x += -dx // тест по оси x
+    //y += dy // тест по оси y
+    if(upPress) {
+        y += -dy 
+    }else if(downPress) {
+        y += dy
+    }else if(leftPress) {
+        x += -dx
+    }else if(rightPress) {
+        x += dx
+    }
 }
 
 // Функция отвечающая за анимирование змейки
 function snakeAnimation() {
-    ctx.clearRect(0,0,canvas.width,canvas.height) // метод для очистки. (x и y у верхнего левого угла, x и y нижнего правого угла)
+    ctx.clearRect(0,0,canvas.width,canvas.height) //метод для очистки. (x и y у верхнего левого угла, x и y нижнего правого угла)
     snake() // отрисовываем змейку
     pass()
     direction()
