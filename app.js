bf49d8145986f73e11139f73772b9d3c49ae2eee
Kir <ekoroteev27@yandex.ru>
2020-11-03 20:18:50 +0300

    add touchscreen

--- app.js
+++ app.js
@@ -7,19 +7,76 @@ let x = 0
 let y = 30
 let dx = 2
 let dy = 2
-
 let upPress = false
 let downPress = false
 let leftPress = false
 let rightPress = false
+let touchStart = null
+let touchPosition = null
+let sensitivity = 20 // Количество пикселей, считающиеся свайпом
 
-document.addEventListener('keydown', KeyDownHandler, false)
+document.addEventListener('keydown', KeyDownHandler, false) // Клавиша клавиатуры зажата (и отпущена)
+document.addEventListener('touchstart', function (e) { TouchStart(e)}) // Начало касания
+document.addEventListener('touchmove', function (e) { TouchMove(e)}) // движение пальца
+document.addEventListener('touchend', function (e) {TouchEnd(e)}) // Конец касания
 
 function keyActivation(up,down,left,right) {
     upPress = up
     downPress = down
     leftPress = left
     rightPress = right
+}
+
+// TouchScreen
+function TouchStart (e) {
+    touchStart = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY}
+    touchPosition = { x: touchStart.x, y: touchStart.y}
+}
+
+function TouchMove(e) {
+    touchPosition = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY}
+}
+
+function TouchEnd(e) {
+    CheckAction()
+    touchStart = null
+    touchPosition = null
+}
+
+function CheckAction() {
+    var d = {
+        x: touchStart.x - touchPosition.x,
+        y: touchStart.y - touchPosition.y
+    }
+    let msg = ''
+
+    if(Math.abs(d.x) > Math.abs(d.y)) {
+        if(Math.abs(d.x) > sensitivity) {
+            if(d.x > 0) {
+                msg = 3
+            } else {
+                msg = 4
+            }
+        }
+    }else{
+        if(Math.abs(d.y) > sensitivity) {
+            if(d.y > 0) {
+                msg = 1
+            } else {
+                msg = 2
+            }
+    }
+}
+    //console.log(msg)
+    if (msg == 1) {
+        keyActivation(true,false,false,false)
+    }else if (msg == 2) {
+        keyActivation(false,true,false,false)
+    }else if (msg == 3) {
+        keyActivation(false,false,true,false)
+    }else if (msg == 4) {
+        keyActivation(false,false,false,true)
+    }
 }
 
 // Кнопки
@@ -39,7 +96,7 @@ function KeyDownHandler(e) {
 function snake() {
     ctx.beginPath()
     // Определяем прямоугольник
-    ctx.rect(x,y,25,50) // (x, y, width, height)
+    ctx.rect(x,y,25,100) // (x, y, width, height)
     ctx.fillStyle = 'white' // указываем цвет прямоугольника
     ctx.fill() // отрисовка
     ctx.closePath()
@@ -72,7 +129,6 @@ function direction() {
         x += dx
     }
 }
-
 
 // Функция отвечающая за анимирование змейки
 function snakeAnimation() {
